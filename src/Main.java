a87e69abb7d7fb45ec5e5750692d7c2ed99253bb
Gautier Pugnonblanc Yann <y.gautier136@laposte.net>
2019-10-17 20:29:26 +0200

    final commit

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ import javax.swing.JSlider;
 public class Main {
 
 	public static void main(String[] args) {
-		JSlider bs;
-		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("RangeSlider");
 		frame.setSize(400, 100);
 
 		RangeSlider le_m = new RangeSlider();
 		le_m.getModel().setExtent(20);
         frame.getContentPane().add(le_m);
-        /*
-        JSlider js = new JSlider();
-        frame.getContentPane().add(js);*/
         
         
         frame.setVisible(true);
